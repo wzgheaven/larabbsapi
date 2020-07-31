c72f8f468c22e947f89de0b51e7859386ef18ee7
wzg <641351447@qq.com>
2020-07-31 10:25:23 +0000

    测试分支

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
 Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+
+
+
