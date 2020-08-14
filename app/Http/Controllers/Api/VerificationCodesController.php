3ca36a30840513e585f0e4c7a54aaa5710635d11
wzg <641351447@qq.com>
2020-08-14 17:10:21 +0000

    发送注册短信验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -14,18 +14,21 @@ class VerificationCodesController extends Controller
     	$phone = $request->phone;
         // 生成4位随机数，左侧补0
         $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
-
-        try {
-            $result = $easySms->send($phone, [
-                'template' => config('easysms.gateways.aliyun.templates.register'),
-                'data' => [
-                    'code' => $code
-                ],
-            ]);
-        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-            $message = $exception->getException('aliyun')->getMessage();
-            abort(500, $message ?: '短信发送异常');
-        }
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+	        try {
+	            $result = $easySms->send($phone, [
+	                'template' => config('easysms.gateways.aliyun.templates.register'),
+	                'data' => [
+	                    'code' => $code
+	                ],
+	            ]);
+	        } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+	            $message = $exception->getException('aliyun')->getMessage();
+	            abort(500, $message ?: '短信发送异常');
+	        }
+	    }
 
         $key = 'verificationCode_'.Str::random(15);
         $expiredAt = now()->addMinutes(5);
