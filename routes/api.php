32f82733ea27800364394b38481b442677e25e18
wzg <641351447@qq.com>
2020-08-16 07:14:04 +0000

    接口频率

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,19 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         return 'this is version v1,-----------';
     })->name('version');
 
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
+    Route::middleware('throttle:'. config('api.rate_limits.sign'), function() {
 
-    Route::post('users', 'UsersController@store')->name('users.store');
+        // 短信验证码
+        Route::post('verificationCodes', 'VerificationCodesController@store')
+            ->name('verificationCodes.store');
+        //用户注册
+        Route::post('users', 'UsersController@store')->name('users.store');
+    });
+    Route::middleware('throttle:'. config('api.rate_limits.access'), function() {
+        
+    });
+
+    
 
 });
 
